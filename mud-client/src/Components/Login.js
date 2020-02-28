18723be01e2608da19d9b470d198e78e1f7dcd68
unknown <dj.zaragoza@icloud.com>
2020-02-27 21:27:19 -0800

    still working on login file.  Creating login layout

--- mud-client/src/Components/Login.js
+++ mud-client/src/Components/Login.js
@@ -1,1 +1,57 @@
-i
+import React, { useReducer } from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state = {
+        username: "",
+        password: "",
+        loggedIn: false,
+        error: false
+    }
+
+    changeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        axios
+            .post('https://', {
+                username: this.state.username,
+                password: this.state.password
+            })
+            .then(res => {
+                window.localStorage.setItem('token', res.data.token);
+                const username = jwt_decode(localStorage.getItem('token')).username;
+                window.localStorage.setItem('username', username);
+                this.setState({
+                    loggedIn: true
+                })
+            })
+            .catch(err => this.setState({
+                error: true
+            }))
+    }
+
+    render() {
+        if(this.state.loggedIn === true) {
+            return <Redirect to='/dashboard'/>
+        } else {
+            return(
+                <div className="login">
+                    <NavBar/>
+                    <h1>Log In to your account</h1>
+                    <Form className="form login-form" onSubmit={this.submitHandler}>
+                        <FormGroup>
+                            <Label>username</Label>
+                            <Input type="string" name="username" value={this.state.username} onChange={this.changeHandler}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Password</Label>
+                            </FormGroup></Form></div>
+            )
+        }
+    }
+}
